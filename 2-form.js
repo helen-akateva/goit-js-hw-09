4e7bdb0b245fad66d752f96871be37916964f42e
helen-akateva <akateva17@gmail.com>
2025-09-05 11:59:49 +0000

    Deploying to gh-pages from @ helen-akateva/goit-js-hw-09@83b75dd8da3e2c3131fccca36f8ba0cbb25cb558 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const e=document.querySelector(".feedback-form");e.addEventListener("submit",r);function r(t){t.preventDedault(),t.target.reset()}
+//# sourceMappingURL=2-form.js.map
